07a368282dc783e84e7d67874c65b2ee0a6dac35
Anurag-Raut <rautab0@gmail.com>
2023-08-01 15:53:14 +0530

    first commit

--- components/card/cards.tsx
+++ components/card/cards.tsx
@@ -1,4 +1,4 @@
-export default function Card({children}) {
+export default function Card({children}:any) {
   console.log(children);
   return (
     <div className="border-2 border-black w-full h-full m-3 p-3 rounded-xl">
--- components/inputText.tsx
+++ components/inputText.tsx
@@ -1,7 +1,7 @@
-export default function InputText({label,onChange,placeholder}){
+export default function InputText({label,onChange,placeholder}:any){
     return (
-        <div class="mb-6">
-            <label for="default-input" class="block mb-2 text-sm font-medium text-gray-600 ">{label}</label>
+        <div className="mb-6">
+            <label htmlFor="default-input" className="block mb-2 text-sm font-medium text-gray-600 ">{label}</label>
             <input onChange={onChange} placeholder={placeholder} type="text" id="default-input" className="bg-base-100 border border-gray-500 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5   " />
         </div>
     )
