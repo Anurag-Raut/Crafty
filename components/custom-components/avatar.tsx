5a2847c2ba600e6a7b72a1e66fa61e008da5fb94
Anurag-Raut <rautab0@gmail.com>
2023-08-06 16:17:41 +0530

    avatar

--- components/custom-components/avatar.tsx
+++ components/custom-components/avatar.tsx
@@ -1,15 +1,23 @@
+import { useUser } from "@auth0/nextjs-auth0/client"
+import Link from "next/link";
+import { useRouter } from "next/router";
 
 export default function Avatar({image,name}){
+    const {user}=useUser();
+    const router = useRouter();
+   
+     
+
 console.log(image,'image')
     return(
-        <div className="avatar flex items-start h-10 ">
+        <Link href={"/api/auth/logout"} className="avatar flex items-start h-10 ">
             <div className="font-bold mr-2"> {name}</div>
             
             <div className="w-10 rounded-full">
               
                 <img src={image} />
             </div>
-        </div>
+        </Link>
     )
 
 }
